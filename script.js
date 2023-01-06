3a2d6863a9745508edd0ef564091a4ee6df4eae4
Daniel <danielmont25@gmail.com>
2023-01-05 20:33:36 -0600

    added code

---
+++ script.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById('submit')
+
+
+submit.addEventListener('click', () => {
+    alert("Thank you for registering. A notice will be sent to your NeuraliD if chosen.")
+})
+
